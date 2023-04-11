21d26e2c9105108dbd138d114a22706794f1be32
Jonathas Aroucha <jonathasfelipe_aroucha@hotmail.com>
2023-04-11 18:46:46 -0300

    adjust of the text and favicon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
             Front-End Developer
           </h2>
           <p className="text-white py-4 max-w-md">
-            I'm Front-End Developer at Compass.UOL.
+          I'm Front-End Developer and future Full-Stack Developer.
           </p>
 
           <div>
